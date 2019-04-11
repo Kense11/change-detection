0a2a13646779ab349fd12f4a2d2119ba64e7f496
Kense11 <kense1maks@gmail.com>
2019-04-11 12:30:59 +0300

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
 
   title = 'change-detection';
 
-  get time() {
-    return  Date.now();
+  time: number;
+
+  constructor(
+  ) {
+    setInterval(() => this.time = Date.now(), 1);
   }
 }
